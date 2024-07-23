9d3606015d4f3d5a4972e92d41f5b92dd6469f20
Oliver Lowe <o@olowe.co>
2024-07-23 15:25:02 +1000

    pcap: add package overview docs

---
+++ pcap/pcap.go
@@ -0,0 +1,5 @@
+// Package pcap implements decoding and encoding of libcap's savefile
+// format described in [pcap-savefile(5)].
+//
+// [pcap-savefile(5)]: https://www.tcpdump.org/manpages/pcap-savefile.5.txt
+package pcap
